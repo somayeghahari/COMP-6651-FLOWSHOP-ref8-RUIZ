791d0b497078f2578e68e09d1ed90e4ec0709999
Letícia Pasdiora <leticiapasdiora@gmail.com>
2020-12-05 23:58:57 -0500

    implemented best permutation

--- src/LocalSearch.java
+++ src/LocalSearch.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class LocalSearch {
@@ -24,8 +25,9 @@ public class LocalSearch {
      */
     public List<Job> IterativeImprovementInsert (List<Job> partialSolution)
     {
-    	NEH neh = new NEH(schedule);
     	schedule = partialSolution;
+    	NEH neh = new NEH(schedule);
+    	
     	boolean improve = true;
     	while (improve)
     	{
@@ -40,10 +42,13 @@ public class LocalSearch {
     			while (checked[id])
     				id = (int) (Math.random() * checked.length);
     			checked[id] = true;
+    			
     			List<Job> partialSchedule = BestPermutation(schedule, id);
+    			
     			if (neh.calculateMakespan(partialSchedule) < neh.calculateMakespan(schedule))
     			{
-    				schedule = partialSchedule;
+    				System.out.println("FOUND DA SHITZ");
+    				schedule = CopyList(partialSchedule);
     				improve = true;
     			}
     		}
@@ -53,8 +58,59 @@ public class LocalSearch {
     	return schedule;
     }
     
+    /**
+     * Method to calculate all possible permutations,
+     * changing the position of the job with the selected id
+     * @param jobs list of jobs
+     * @param id id of the job to do the permutation
+     * @return the permutation with lowest makespan
+     */
     public List<Job> BestPermutation(List<Job> jobs, int id)
     {
-    	return jobs;
+    	List<Job> permutation = CopyList(jobs);
+    	Job job = permutation.remove(id);
+    	permutation.add(0, job);
+    	
+    	NEH neh = new NEH(permutation);
+    	int makespan = neh.calculateMakespan(permutation);
+    	int opt = 0; // store the optimal position for the randomly selected job j
+    	
+    	for (int i = 1; i < permutation.size(); i++)
+    	{
+    		permutation.remove(i-1);
+        	permutation.add(i, job);
+        	int partialMakespan = neh.calculateMakespan(permutation);
+        	
+        	
+        	if (partialMakespan < makespan)
+        	{
+        		opt = i;
+        		makespan = partialMakespan;
+        	}
+    	}
+
+    	permutation.remove(permutation.size()-1);
+    	permutation.add(opt, job);
+    	
+    	return permutation;
+    }
+    
+    public void CheckAcceptanceCriteria()
+    {
+    	
+    }
+    
+    /**
+     * Method to copy the list into a new one (different pointers needed)
+     * @param copy the list you want to copy
+     * @return the list copied to another pointer
+     */
+    public List<Job> CopyList (List<Job> copy)
+    {
+    	List<Job> result = new ArrayList<Job>();
+    	for (int i = 0; i < copy.size(); i++)
+    		result.add(copy.get(i));
+    	
+    	return result;
     }
 }
